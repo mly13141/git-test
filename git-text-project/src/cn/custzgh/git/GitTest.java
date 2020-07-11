6f2574972093a64ce6edf06f5456039db654ebcf
MLY <1491477074@qq.com>
2020-07-11 19:48:18 +0800

    把本地工程添加到仓库

---
+++ git-text-project/src/cn/custzgh/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.custzgh.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
