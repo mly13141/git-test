c08a15708e8c19509d29da097cee4b5179b9da43
MLY <1491477074@qq.com>
2020-07-11 19:49:52 +0800

    第一次修改GitTest,添加了test方法

--- git-text-project/src/cn/custzgh/git/GitTest.java
+++ git-text-project/src/cn/custzgh/git/GitTest.java
@@ -9,4 +9,7 @@ public class GitTest {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+    public void test(){
+        System.out.println("sssss");
+    }
 }
